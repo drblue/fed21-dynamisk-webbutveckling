066e5642ce417d16c9d36648abe24511ff16d200
Johan Nordström <johan@digitalvillage.se>
2022-02-01 11:02:26 +0100

    Global-object in node.

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,15 @@
+/**
+ * Globals
+ */
+
+console.log("Hello, world 🌱🌍!");
+
+setTimeout(() => {
+	console.log("I'll be back!");
+}, 3000);
+
+let counter = 0;
+setInterval(() => {
+	counter += 2;
+	console.log(`Hello after ${counter} seconds`);
+}, 2000);
