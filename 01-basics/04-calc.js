356181787fcac308a659d44f3f48d852b17b55b3
Johan Nordström <johan@digitalvillage.se>
2022-02-01 15:31:42 +0100

    A rounding function, AS MATH.ROUND SHOULD HAVE WORKED!

--- 01-basics/04-calc.js
+++ 01-basics/04-calc.js
@@ -3,16 +3,14 @@
  */
 
 const geo = require('./modules/geometry');
+const num = require('./modules/num');
 
 // console.log("geo", geo);
 
 let radius = 4;
 
 let area = geo.area(radius);
-// let approxArea = Math.round(area * 10) / 10;
-// let approxArea = num.roundWith1Decimal(area); // * 10/10
-// let approxArea = num.roundWith2Decimals(area); // * 100/100
-// let approxArea = num.round(area, 4);
+let approxArea = num.round(area, 8);
 
 console.log(`The area of a circle with radius ${radius} is:`, area);
 console.log(`The approx. area of a circle with radius ${radius} is:`, approxArea);
