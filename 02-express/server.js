d06d8182a6534e3cd10325fa37651298d0ea0854
Johan Nordström <johan@digitalvillage.se>
2022-02-02 11:57:45 +0100

    Modified response.

--- 02-express/server.js
+++ 02-express/server.js
@@ -5,6 +5,16 @@
 const express = require('express');
 const app = express();
 
+// When a GET request for `/` (http://localhost:3000/) is received, run this function
+app.get('/', (req, res) => {
+	// req = information om den inkommande förfrågan
+	// res = metoder för att skicka ett svar på förfrågan
+	console.log("Someone requested my root!");
+
+	res.send('Hello I AM HUNGRY 😋🍽!');
+});
+
+// Start listening for incoming requests on port 3000
 app.listen(3000, () => {
 	console.log(`🥳 Yay, server started at http://localhost:3000`);
 });
