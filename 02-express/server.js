5026220449657957c8c96a7ceb7298333d88a04a
Johan Nordström <johan@digitalvillage.se>
2022-02-02 11:00:28 +0100

    Start a new Express app and set it to listen for requests at port 3000.

---
+++ 02-express/server.js
@@ -0,0 +1,10 @@
+/**
+ * express
+ */
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+	console.log(`🥳 Yay, server started at http://localhost:3000`);
+});
