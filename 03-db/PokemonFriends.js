ca97d52444724d4200aca0804eba9bfbba868841
Martin Haagen <martin@mbh.se>
2022-02-11 17:10:11 +0100

    Sista tillläggen efter workshop

---
+++ 03-db/PokemonFriends.js
@@ -0,0 +1,7 @@
+const bookshelf = require('./bookshelf');
+
+const PokemonFriends = bookshelf.Model.extend({
+    tableName: "PokemonFriends"
+});
+
+module.exports = PokemonFriends;
--- 03-db/orm.js
+++ 03-db/orm.js
@@ -1,5 +1,5 @@
 const PokemonCards = require('./PokemonCards');
-//const PokemonFriends = require('./PokemonFriends');
+const PokemonFriends = require('./PokemonFriends');
 
 /*
 PokemonCards.fetchAll().then((collection) => {
@@ -21,7 +21,7 @@ PokemonCards.where(parametrar).fetchAll().then((collection) => {
     console.log(collection.toJSON());
 });
 */
-
+/*
 const attribut = {
     name: "Skrelp",
     hp: 50
@@ -29,6 +29,42 @@ const attribut = {
 let Skrelp = new PokemonCards(attribut);
 Skrelp.save().then((res) => {
     console.log(res);
+});
+PokemonCards.fetchAll().then((collection) => {  
+    collection.forEach((model) => {
+        console.log(`Name: ${model.get('name')} HP: ${model.get('hp')}`);
+    });
+});
+*/
+/*
+PokemonFriends.fetchAll().then((result) => {
+    console.log(result.toJSON());
+});
+*/
+/*
+for(var i=1;i<11;i++) {
+    const attribute = {
+        name: "namn"+i,
+        email: "namn"+i+"@email.nu"
+    };
+    let Friend = new PokemonFriends(attribute);
+    Friend.save().then((res) => {
+        console.log(`User ${res.get('name')} was successfully saved with id=${res.id}`);
+    });
+}
+*/
+
+
+// Hämta alla PokemonFriends som har ett namn som börjar på "namn"
+// SELECT * FROM PokmonFriends WHERE name LIKE 'name%' 
+PokemonFriends.where("name", "LIKE", "namn%").fetchAll().then((collection) => {
+    console.log(collection.toJSON());
+    // Loopa igenom alla resultaten
+    collection.forEach((model) => {
+        // Radera dem en efter en
+        model.destroy().then();
+    });
+    
 });
 
 // destroy() -- för att radera 
