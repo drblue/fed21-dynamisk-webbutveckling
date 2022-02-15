f898302612d44eb1dc2392cbd6439335707458c8
Martin Haagen <martin@mbh.se>
2022-02-15 08:35:21 +0100

    Added pokemon database v1

---
+++ 04-pokemon/sql/db.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE PokemonFriends (
+     id INTEGER NOT NULL AUTO_INCREMENT,
+	 name VARCHAR(250),
+     email VARCHAR(250),
+     PRIMARY KEY(id)
+);
+
+CREATE TABLE PokemonCards (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	name VARCHAR(250),
+	hp INTEGER NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE PokemonBattles (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	winningPokemon INTEGER NOT NULL,	
+	loosingPokemon INTEGER NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(winningPokemon) REFERENCES PokemonCards(id),
+	FOREIGN KEY(loosingPokemon) REFERENCES PokemonCards(id)
+);
