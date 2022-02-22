5a1f22d619f79ebe4547b16befb68a8f9539cc5c
Johan Nordström <johan@digitalvillage.se>
2022-02-22 16:17:08 +0100

    Added custom validator for username when creating a new user.

--- 06-books/validation/user.js
+++ 06-books/validation/user.js
@@ -3,9 +3,17 @@
  */
 
 const { body } = require('express-validator');
+const models = require('../models');
 
 const createRules = [
-	body('username').exists().isLength({ min: 3 }),
+	body('username').exists().isLength({ min: 3 }).custom(async value => {
+		const user = await new models.User({ username: value }).fetch({ require: false });
+		if (user) {
+			return Promise.reject("Username already exists.");
+		}
+
+		return Promise.resolve();
+	}),
 	body('password').exists().isLength({ min: 4 }),
 	body('first_name').exists().isLength({ min: 2 }),
 	body('last_name').exists().isLength({ min: 2 }),
