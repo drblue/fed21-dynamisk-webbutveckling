dc7468a9146635c0423b7d09fb6b005d115ec2ad
Johan Nordström <johan@digitalvillage.se>
2022-02-22 13:56:39 +0100

    Added express-validator and a POST `/test` route that validates incoming body data.

--- 06-books/app.js
+++ 06-books/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
+const { body, validationResult } = require('express-validator');
 
 // instantiate express
 const app = express();
@@ -10,6 +11,17 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.post('/test', [
+	body('name').exists().isLength({ min: 3 }),
+], (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).send({ status: 'fail', data: errors.array() });
+	}
+
+	res.send({ status: 'success', data: req.body});
+});
 
 // routes
 app.use(require('./routes'));
