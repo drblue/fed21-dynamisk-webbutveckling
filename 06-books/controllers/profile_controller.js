8c41ef43f5c9e4135f9580964bf9df72972cda00
Johan Nordström <johan@digitalvillage.se>
2022-02-23 10:53:15 +0100

    Added `/profile` routes and controller.

---
+++ 06-books/controllers/profile_controller.js
@@ -0,0 +1,54 @@
+/**
+ * Profile Controller
+ */
+
+const debug = require('debug')('books:profile_controller');
+const { matchedData, validationResult } = require('express-validator');
+const models = require('../models');
+
+/**
+ * Get authenticated user's profile
+ *
+ * GET /
+ */
+const getProfile = async (req, res) => {
+	// somehow get the authenticated user
+	// and return it
+
+	res.send({
+		status: 'success',
+		data: {
+			user: null,
+		}
+	});
+}
+
+/**
+ * Update authenticated user's profile
+ *
+ * PUT /
+ */
+const updateProfile = async (req, res) => {
+	res.status(405).send({
+		status: 'error',
+		message: 'This is a workshop.',
+	});
+}
+
+/**
+ * Get authenticated user's books
+ *
+ * GET /books
+ */
+const getBooks = async (req, res) => {
+	res.status(405).send({
+		status: 'error',
+		message: 'This is a workshop.',
+	});
+}
+
+module.exports = {
+	getProfile,
+	updateProfile,
+	getBooks,
+}
---
+++ 06-books/routes/profile.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const profileController = require('../controllers/profile_controller');
+// const profileValidationRules = require('../validation/profile');
+
+/**
+ * Get authenticated user's profile
+ */
+router.get('/', profileController.getProfile);
+
+/**
+ * Update authenticated user's profile
+ *
+ * @todo Add validation rules
+ */
+router.put('/', profileController.updateProfile);
+
+/**
+ * Get authenticated user's books
+ */
+router.get('/books', profileController.getBooks);
+
+module.exports = router;
