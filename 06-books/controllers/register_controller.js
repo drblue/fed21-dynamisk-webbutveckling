a04acb9a4349815a473591b5fd86908083997a68
Johan Nordström <johan@digitalvillage.se>
2022-02-25 13:23:47 +0100

    Added hashing of password when registering a new user.

--- 06-books/controllers/register_controller.js
+++ 06-books/controllers/register_controller.js
@@ -2,6 +2,7 @@
  * Register Controller
  */
 
+const bcrypt = require('bcrypt');
 const debug = require('debug')('books:register_controller');
 const { matchedData, validationResult } = require('express-validator');
 const models = require('../models');
@@ -22,6 +23,19 @@ const register = async (req, res) => {
 	const validData = matchedData(req);
 
 	console.log("The validated data:", validData);
+
+	// generate a hash of `validData.password`
+	// and overwrite `validData.password` with the generated hash
+	try {
+		validData.password = await bcrypt.hash(validData.password, 10);
+
+	} catch (error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown when hashing the password.',
+		});
+		throw error;
+	}
 
 	try {
 		const user = await new models.User(validData).save();
